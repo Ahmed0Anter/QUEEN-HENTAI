04738392a53711eadbeefaf5baa8c109b55ae35f
Ahmed0Anter <159495941+Ahmed0Anter@users.noreply.github.com>
2024-02-15 23:27:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['500460873', 'Ahmed', true], 
   [''] 
 ] //Number of owners
 
@@ -47,19 +46,19 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '💝 Queen Hentai 💝'
+global.botname = 'KILLER'
 global.premium = 'true'
-global.packname = '💝 Queen Hentai 💝 Created By 💝 Dinuwa Official²⁰²³ 💝' 
-global.author = '💝 Dinuwa Official²⁰²³ 💝' 
-global.igfg = '▢ Follow on Facebook\https://www.facebook.com/profile.php?id=61551915615888\n' 
-global.dygp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
-global.fgsc = 'https://github.com/dinuwah/QUEEN-HENTAI' 
-global.fgyt = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
-global.fgpyp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
-global.fglog = 'https://i.ibb.co/T1DqdYz/thumbnail.jpg' 
+global.packname = '💝 KILLLER 💝 Created By 💝 Ahmed 💝' 
+global.author = '💝 Ahmed 💝' 
+global.igfg = '💝 KILLLER 💝 Created By 💝 Ahmed 💝' 
+global.dygp = '💝 KILLLER 💝 Created By 💝 Ahmed 💝'
+global.fgsc = '💝 KILLLER 💝 Created By 💝 Ahmed 💝' 
+global.fgyt = '💝 KILLLER 💝 Created By 💝 Ahmed 💝'
+global.fgpyp = '💝 KILLLER 💝 Created By 💝 Ahmed 💝'
+global.fglog = '💝 KILLLER 💝 Created By 💝 Ahmed 💝' 
 
 
-global.wait = '*💝 _Waiting Bro Uploading by 💝 Queen Hentai 💝..._*'
+global.wait = '*💝 _Waiting Bro Uploading by 💝 KILLER 💝..._*'
 global.rwait = '🇱🇰'
 global.dmoji = '🤣'
 global.done = '👍'
